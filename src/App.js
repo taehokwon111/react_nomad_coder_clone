841996117c6d160f4512d917319d6e736d4aedd3
unknown <52158699+Cancermaker@users.noreply.github.com>
2021-04-14 15:09:59 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Potato from "./Potato";
+
 
 function App() {
-  return <div className="App"><h1>임 주 녕</h1></div>
+  return <div className="App"><h1>임 주 녕</h1><Potato/></div>
 
 }
 
---
+++ src/Potato.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Potato(){
+    return <h1>I love Potato</h1>;
+}
+
+export default Potato;
