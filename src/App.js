7930de002a75324f6eafbd836e8e2f31af147c1b
unknown <52158699+Cancermaker@users.noreply.github.com>
2021-05-11 19:22:57 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import Potato from "./Potato";
 
-
-function App() {
-  return <div className="App"><h1>임 주 녕</h1><Potato/></div>
-
+function Food({fav}){
+  return <h1>나는 {fav}가 조아요</h1>
 }
 
+function App() {
+  return ( 
+  <div className="App">
+    <h1>임 주 녕</h1>
+    <Food fav = "kimchi"/>
+    <Food fav = "raman"/>
+    <Food fav = "you"/>
+    <Food fav = "rice"/>
+    </div>
+    )
+
+}
+ 
 export default App;
